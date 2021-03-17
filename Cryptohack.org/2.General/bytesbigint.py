9894d0aa12555734309a7ff210cc5c0c33f13c36
fesgic <festusgichohi2@gmail.com>
2021-03-17 14:12:35 +0300

    updated code

--- Cryptohack.org/2.General/bytesbigint.py
+++ Cryptohack.org/2.General/bytesbigint.py
@@ -4,5 +4,6 @@ import Crypto.Util.number
 interger = int(input("Enter your interger here: "))
 
 byte = interger.to_bytes(50,'big')
+#we use a big value value here to avoid an overflow
 flag = byte.decode("ascii")
 print(flag)
