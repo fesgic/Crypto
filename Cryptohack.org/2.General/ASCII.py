b21f5d826a9b3ca7878ab385c30476bca61f994e
fesgic <festusgichohi2@gmail.com>
2021-03-18 17:07:53 +0300

    formated. Prints flag in a line

--- Cryptohack.org/2.General/ASCII.py
+++ Cryptohack.org/2.General/ASCII.py
@@ -1,9 +1,4 @@
 #!/bin/python
 integer = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 print(integer)
-for i in integer:
-    ascii = chr(i)
-    print(ascii)
-
-
-    
+print(''.join(chr(x) for x in integer))
